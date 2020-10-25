ae3fb00546fd3769e2fdff3ba09ecea0abe1808d
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-25 23:00:07 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -48,15 +48,15 @@ function changeTheme(event) {
       }
 }
 
-// function chosenTheme(){
-//     const chosen = JSON.parse(localStorage.getItem('current-theme'));
-//     themeSwitcher.checked = chosen == 'dark-theme' ? true : false;
-//     if (themeSwitcher.checked) {
-//        bodyTheme.classList.add(Theme.DARK);
-//     } 
-// }
+function chosenTheme(){
+    const chosen = JSON.parse(localStorage.getItem('current-theme'));
+    themeSwitcher.checked = chosen == 'dark-theme' ? true : false;
+    if (themeSwitcher.checked) {
+       bodyTheme.classList.add(Theme.DARK);
+    } 
+}
 
-// chosenTheme()
+chosenTheme()
 
 const createMenu = menuTemplate(menuList);
 menuContainer.insertAdjacentHTML("beforeend", createMenu);
