e4dc38b3660a560203e416e2aec0d4c918895ea8
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-25 18:05:23 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -5,17 +5,24 @@ import menuTemplate from '../templates/menu-card.hbs';
 
 const menuContainer = document.querySelector(".js-menu");
 const themeSwitcher = document.querySelector('#theme-switch-toggle')
+const bodyTheme = document.querySelector("body")
+themeSwitcher.addEventListener('change', changeTheme)
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+function changeTheme(event) {
+    
+}
+
 const createMenu = menuTemplate(menuList);
 menuContainer.insertAdjacentHTML("beforeend", createMenu);
 
-console.log(themeSwitcher);
+// console.log(themeSwitcher);
 // console.log(createMenu);
 // console.log(menuTemplate);
 // console.log(menuList);
 // console.log('test message');
 // console.log(menuContainer);
+console.log(bodyTheme);
