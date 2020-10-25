537464b21afb473d4e88d691faaaeb37241c8504
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-25 16:58:05 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import './styles.css';
 import menuList from './menu.json';
 import menuTemplate from '../templates/menu-card.hbs';
 
+
+const menuContainer = document.querySelector(".js-menu");
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
 const createMenu = menuTemplate(menuList);
+
 console.log(createMenu);
 console.log(menuTemplate);
 console.log(menuList);
 console.log('test message');
+console.log(menuContainer);
