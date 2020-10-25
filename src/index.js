78acfce81eaaf92df69e3183832be15d031e4a85
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-25 19:12:09 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const Theme = {
 function changeTheme(event) {
     
     bodyTheme.classList.add(Theme.DARK)
-    themeSwitcher.inputEl.checked;
+    themeSwitcher.checked=true;
     localStorage.setItem('current-theme', JSON.stringify(Theme.DARK))
 }
 
