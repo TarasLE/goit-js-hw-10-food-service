03739d776791c522655865ffb167fbbbddee8c1b
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-25 14:27:44 +0200

    interin save

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+import menuList from './menu.json';
+import menuTemplate from '../templates/menu-card.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const createMenu = menuTemplate(menuList);
+console.log(createMenu);
+console.log('test message');
