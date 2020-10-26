3c71b34cb4b7605673cdab5b200d78a8a5818b50
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-10-26 22:58:19 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import { menuContainer } from './selectors';
 import { themeSwitcher } from './selectors';
 import { bodyTheme } from './selectors';
 
-// const menuContainer = document.querySelector(".js-menu");
-// const themeSwitcher = document.querySelector('#theme-switch-toggle')
-// const bodyTheme = document.querySelector("body")
 themeSwitcher.addEventListener('change', changeTheme)
-
 
 const Theme = {
   LIGHT: 'light-theme',
--- src/selectors.js
+++ src/selectors.js
@@ -1,4 +1,3 @@
-export default {}
 export const menuContainer = document.querySelector(".js-menu");
 export const themeSwitcher = document.querySelector('#theme-switch-toggle');
 export const bodyTheme = document.querySelector("body");
